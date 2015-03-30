36c3ed3c04c72d8b06300320722ee09e1354e657
Sean Melody <smelody@rallydev.com>
2015-03-30 11:55:17 -0400

    Added backend for features

--- public/app/src/VoteNow.js
+++ public/app/src/VoteNow.js
@@ -1,18 +1,33 @@
-export class VoteNow {
-  constructor(){
-    this.hello = 'Welcome to the Aurelia Navigation App!';
-    this.features = this.getFeatures();
-    this.feature1 = this.features[0];
-    this.feature2 = this.features[1];
-  }
+import {HttpClient} from 'aurelia-http-client';
+var url = "http://localhost:7000/api/features";
 
-  getFeatures () {
-  	return [{
-  		name: "Rebuild the hoover dam",
-  		description: "I think we should rearchitect it."
-  	}, {
-  		name: "Give my cat a bath",
-  		description : "The cat hates water but needs a bath"
-  	}];
+export class VoteNow {
+
+	static inject() { return [HttpClient]; }
+	constructor(http){
+		this.hello = 'Welcome to the Aurelia Navigation App!';
+
+        this.features = [];
+    	this.feature1 = {};
+    	this.feature2 = {};
+		this.http = http;
+	} 
+
+	activate(){
+	  	return this.http.get(url).then(response => {
+	     
+	        this.features = response.content.features;
+	    	this.feature1 = this.features[0];
+	    	this.feature2 = this.features[1];
+	    });
+  	}
+  	getFeatures () {
+	  	return [{
+	  		name: "Rebuild the hoover dam",
+	  		description: "I think we should rearchitect it."
+	  	}, {
+	  		name: "Give my cat a bath",
+	  		description : "The cat hates water but needs a bath"
+	  	}];
   }
 }
--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,19 @@ router.get('/api/demo', function(req, res) {
   res.json({ msg: 'From the Node-Backend'});
 });
 
+router.get('/api/features', function(req, res) {
+	var features = [{
+  		name: "Rebuild the hoover dam",
+  		description: "I think we should rearchitect it.  Old stuff cannot be as awesome as new stuff"
+  	}, {
+  		name: "Give my cat a bath",
+  		description : "The cat hates water but needs a bath"
+  	}];
+	res.json( {features: features} );
+});
+
+
+
 router.get('/', function(req, res) {
   res.redirect('/app/');
 });
